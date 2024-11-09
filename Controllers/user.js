27ed9890be4a1f15dbd28bdc46fe0922865cedf4
prakrati <prakratipal21439@gmail.com>
2024-11-10 00:48:30 +0530

    first commit

---
+++ Controllers/user.js
@@ -0,0 +1,51 @@
+const User = require('../Modals/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const cookieOptions = {
+    httpOnly: true,
+    secure: false, // Set to true in production
+    sameSite: 'Lax'
+  
+};
+
+exports.signUp = async(req,res)=>{
+    try{
+        const { channelName, userName, about, profilePic, password } = req.body;
+        const isExist = await User.findOne({ userName });
+        if (isExist){
+            res.status(400).json({error:"Username Already Exist Please try with other Username"})
+        }else{
+            let updatedPass =await bcrypt.hash(password,10);
+            const user =new User({ channelName, userName , about, profilePic, password:updatedPass});
+            await user.save();
+            res.status(200).json({message:"User Created Successfully", success:"yes",data:user});
+        }
+        
+    } catch (error){
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+exports.signIn = async (req,res)=>{
+    try{
+        const { userName, password } = req.body;
+        const user = await User.findOne({ userName });
+
+        if(user && await bcrypt.compare(password, user.password)){
+            const token = jwt.sign({ userId: user._id }, 'Its_My_Secret_Key');
+            res.cookie('token',token, cookieOptions);
+            res.json({message:'Logged in successfully', success:"true", token,user});
+        }
+        else{
+            res.status(400).json({error:"Invalid Credentials"})
+        }
+    } catch (errorMsg){
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+exports.logout = async(req,res)=>{
+    res.clearCookie('token', cookieOptions).json({ message: 'Logged out successfully' });
+}
